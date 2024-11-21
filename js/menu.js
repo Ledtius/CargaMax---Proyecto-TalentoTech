e8a6985659772e7eb8b25e3799101830f9c4de81
Calet Devia <calebtius@gmail.com>
2024-11-20 19:53:05 -0500

    Add the modal in the contact form

--- js/menu.js
+++ js/menu.js
@@ -19,3 +19,60 @@ window.addEventListener("scroll", () => {
     contactContent.classList.remove("oculto");
   }
 });
+
+/* Contacto boton y modal */
+
+const btn = document.getElementById("contact__btn");
+const allInputs = document.querySelectorAll(".contact__form-label");
+
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const correo = document.getElementById("correo");
+const telefono = document.getElementById("telefono");
+const mensaje = document.getElementById("mensaje");
+const modal = document.getElementById("modal");
+const btnModal = document.querySelector(".contact__modal-btn");
+
+console.log(btnModal);
+console.log(telefono);
+console.log(apellido);
+console.log(nombre);
+console.log(mensaje);
+console.log(correo);
+
+function clickBtn() {
+  event.preventDefault();
+  const nombreValue = nombre.value;
+  const apellidoValue = apellido.value;
+  const correoValue = correo.value;
+  const telefonoValue = telefono.value;
+  const mensajeValue = mensaje.value;
+
+  if (
+    !nombreValue ||
+    !apellidoValue ||
+    !correoValue ||
+    !telefonoValue ||
+    !mensajeValue
+  ) {
+    console.log("return;");
+    return;
+  } else {
+    modal.style.opacity = "1";
+    modal.style.pointerEvents = "unset";
+  }
+}
+
+function closeBtn() {
+  modal.style.opacity = "0";
+  modal.style.pointerEvents = "none";
+
+  nombre.value = "";
+   apellido.value = "";
+   telefono.value = "";
+   correo.value = "";
+   mensaje.value = "";
+}
+
+btn.addEventListener("click", clickBtn);
+btnModal.addEventListener("click", closeBtn);
