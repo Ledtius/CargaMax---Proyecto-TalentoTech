53d3b8f08eda36edaa47ab4ba170b2d95f0e5691
Calet Devia <calebtius@gmail.com>
2024-11-22 18:23:23 -0500

    Fixed the position fixed of the text in the calculator section and eliminate te input of the calculator when show the result

--- js/calculator.js
+++ js/calculator.js
@@ -58,6 +58,11 @@ function clickForm() {
     (distanciaValue / consumoVehicularValue) * precioCombustibleValue;
 
   resultado.textContent = "$" + constoTotal + " COP";
+
+  distancia.value = "";
+  consumoVehicular.value = "";
+  precioCombustible.value = "";
+
 }
 
 btnCalculator.addEventListener("click", clickForm);
