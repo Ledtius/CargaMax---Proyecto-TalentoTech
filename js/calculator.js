d31ccb5e38af586edd1889f1bfab21fe9f23f494
Calet Devia <calebtius@gmail.com>
2024-12-02 20:41:37 -0500

    Changed names of the unids in the html and finished the principal logic of the calculator

--- js/calculator.js
+++ js/calculator.js
@@ -26,6 +26,39 @@ const selectorUnidadesPrecio = document.getElementById("unidades-p/c");
 
 const inputPrecio = document.getElementById("precio-combustible");
 
+const resultadoPantalla = document.getElementById("resultado");
+
+function observadorOpcionDistancia() {
+  let opcionSeleccionada =
+    selectorUnidadesDistancia.options[selectorUnidadesDistancia.selectedIndex];
+
+  opcionSeleccionadaValue = opcionSeleccionada.textContent.trim();
+
+  // console.log(opcionSeleccionadaValue);
+
+  return opcionSeleccionadaValue;
+}
+
+function observadorOpcionConsumo() {
+  let opcionSeleccionada =
+    selectorUnidadesConsumo.options[selectorUnidadesConsumo.selectedIndex];
+
+  opcionSeleccionadaValue = opcionSeleccionada.textContent.trim();
+
+  return opcionSeleccionadaValue;
+}
+
+function observadorOpcionPrecio() {
+  let opcionSeleccionada =
+    selectorUnidadesPrecio.options[selectorUnidadesPrecio.selectedIndex];
+
+  let opcionSeleccionadaValue = opcionSeleccionada.textContent.trim();
+
+  return opcionSeleccionadaValue;
+}
+
+// selectorUnidadesDistancia.addEventListener("change", observadorOpcionDistancia);
+
 calculator.addEventListener("submit", function () {
   event.preventDefault();
 
@@ -35,7 +68,13 @@ calculator.addEventListener("submit", function () {
   const inputConsumoValue = Number(inputConsumo.value);
   const inputPrecioValue = Number(inputPrecio.value);
 
-  /* Convercion de unidades */
+  /* Declaracion de varibles de unidades */
+
+  let valorDistancia;
+  let valorConsumo;
+  let valorPrecio;
+
+  /* Defino las variables de convercion de unidades */
 
   let km = inputDistanciaValue;
   let mi = km * 1.60934;
@@ -46,10 +85,58 @@ calculator.addEventListener("submit", function () {
   let copPorL = inputPrecioValue;
   let copPorGal = copPorL * 3.78541;
 
-  console.log(`Km: ${km}`);
-  console.log(`Mi: ${mi}`);
-  console.log(`Km/L: ${kmPorL}`);
-  console.log(`L/100Km: ${LPor100Km}`);
-  console.log(`COP$/L: ${copPorL}`);
-  console.log(`COP$/Gal: ${copPorGal}`);
+  // console.log(`Mi: ${mi}`);
+  // console.log(`Km/L: ${kmPorL}`);
+  // console.log(`L/100Km: ${LPor100Km}`);
+  // console.log(`COP$/L: ${copPorL}`);
+  // console.log(`COP$/Gal: ${copPorGal}`);
+
+  /*Almaceno la unidad de la f(x) en la variable */
+
+  let opcionDistancia = observadorOpcionDistancia();
+
+  console.log(`Seleccionaste: ${opcionDistancia}`);
+
+  /* Se hace la covercion */
+
+  if (opcionDistancia === "Km") {
+    /* Se llama la variable unidad y se hace la convercion */
+
+    valorDistancia = km;
+    console.log(`Km: ${valorDistancia}`);
+  } else {
+    valorDistancia = mi;
+    console.log(`Mi: ${valorDistancia}`);
+  }
+
+  let opcionConsumo = observadorOpcionConsumo();
+
+  console.log(`Seleccionaste: ${opcionConsumo}`);
+
+  if (opcionConsumo === "Km/L") {
+    valorConsumo = kmPorL;
+    console.log(`Km/L: ${valorConsumo}`);
+  } else {
+    valorConsumo = LPor100Km;
+    console.log(`L/100Km: ${valorConsumo}`);
+  }
+
+  let opcionPrecio = observadorOpcionPrecio();
+
+  console.log(`Seleccionaste: ${opcionPrecio}`);
+
+  if (opcionPrecio === "COP$/L") {
+    valorPrecio = copPorL;
+    console.log(`COP$/L: ${valorPrecio}`);
+  } else {
+    valorPrecio = copPorGal;
+    console.log(`COP$/Gal: ${valorPrecio}`);
+  }
+
+  let resultado = (valorDistancia / valorConsumo) * valorPrecio;
+
+  resultadoPantalla.innerHTML = resultado;
 });
+
+/* NOTA: */
+/* Para los estilos si tengo que usar un addEventListener para cambiar el laberl y el placeholder del input */
